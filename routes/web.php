b9706515b89904feb567657ac9589fbbd1fc8c77
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-03-05 16:02:25 +0200

    Home Page Controller for Frontend Views

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
 
-Route::get('/', function () {
-    return view('frontend.home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,8 +21,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
 
 
 require __DIR__ . '/auth.php';
