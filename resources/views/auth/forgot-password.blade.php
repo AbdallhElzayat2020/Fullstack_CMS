0bdedc26a0e5b90f9ad9a3faea6f165003465021
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-03-28 05:43:53 +0200

    Forget password frontend

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,70 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+{{--<x-guest-layout>--}}
+{{--    <div class="mb-4 text-sm text-gray-600">--}}
+{{--        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}--}}
+{{--    </div>--}}
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+{{--    <!-- Session Status -->--}}
+{{--    <x-auth-session-status class="mb-4" :status="session('status')" />--}}
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+{{--    <form method="POST" action="{{ route('password.email') }}">--}}
+{{--        @csrf--}}
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+{{--        <!-- Email Address -->--}}
+{{--        <div>--}}
+{{--            <x-input-label for="email" :value="__('Email')" />--}}
+{{--            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />--}}
+{{--            <x-input-error :messages="$errors->get('email')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <div class="flex items-center justify-end mt-4">--}}
+{{--            <x-primary-button>--}}
+{{--                {{ __('Email Password Reset Link') }}--}}
+{{--            </x-primary-button>--}}
+{{--        </div>--}}
+{{--    </form>--}}
+{{--</x-guest-layout>--}}
+
+
+@extends('frontend.layouts.master')
+@section('title', __('Login'))
+
+@section('content')
+
+    <section class="wrap__section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mx-auto py-5" style="max-width: 380px;">
+                        <div class="card-body">
+                            <h4 class="card-title mb-4">{{__('Forget Password')}}</h4>
+
+                            {{--check session for alert--}}
+                            <x-auth-session-status class="mb-4 text-success" :status="session('status')"/>
+
+                            <form action="{{ route('password.email') }}" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <input class="form-control" type="email" name="email" placeholder="email" value="{{old('email')}}" required autofocus>
+                                    @error('email')
+                                    <p class="text-danger">
+                                        {{ $message }}
+                                    </p>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">{{__('Reset Link')}}</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <p class="text-center mt-4 mb-0">{{__('Login to you Account')}} <a class="text-primary" href="{{ route('login') }}">{{__('Login in')}}</a></p>
+
+                </div>
+            </div>
         </div>
+    </section>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
+
